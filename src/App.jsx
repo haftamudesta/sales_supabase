69b3e018d91c2c75ee58dcae4450cd0962a81ece
haftamudesta <desta451616@hotmail.com>
2026-07-22 12:05:03 -0700

    Add supabase connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Dashboard } from "./components/Dasnboard";
+
 function App() {
   return (
     <>
       <div>
         <h1 className="text-center text-emerald-400 text-3xl">Supabas App</h1>
       </div>
+      <Dashboard />
     </>
   );
 }
