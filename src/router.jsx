4cc13c7f5c8945b9bbe14ea1f8310f9934d5f65a
haftamudesta <desta451616@hotmail.com>
2026-07-24 10:27:09 -0700

    Update router

--- src/router.jsx
+++ src/router.jsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Dashboard } from "./routes/Dasnboard";
 import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
   },
   {
     path: "/dashboard",
